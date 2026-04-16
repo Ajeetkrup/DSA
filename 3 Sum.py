e5688f34fe3a20c1af021d0e2e7a2c92c9f17f30
ajeetkrup <ajeetkrup401gmail.com>
2026-04-16 23:42:25 +0530

    3 sum unptimized 2

--- 3 Sum.py
+++ 3 Sum.py
@@ -14,3 +14,25 @@ class Solution:
                         seen.add(tuple(triplet))
 
         return result
+
+class Solution:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        result = []
+        seen = set()
+        map_list = {}
+
+        for i in (range(len(nums))):
+            map_list[nums[i]] = i
+
+        for i in (range(len(nums))):
+            for j in range(i+1, len(nums)):
+                k = map_list.get(-(nums[i]+nums[j]))
+                if k != None and k != i and i != j and j != k:
+                    triplet = [-(nums[i]+nums[j]), nums[i], nums[j]]
+                    triplet.sort()
+
+                    if tuple(triplet) not in seen:
+                        result.append([nums[i], nums[j], -(nums[i]+nums[j])])
+                        seen.add(tuple(triplet))
+
+        return result
